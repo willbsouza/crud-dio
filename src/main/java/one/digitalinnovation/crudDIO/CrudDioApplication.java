012acb2d53f51e5dcb8b643a38b8d01cfca29bca
Willams Bernardo <willamsbernardo@hotmail.com>
2021-10-26 20:40:25 -0300

    First commit

---
+++ src/main/java/one/digitalinnovation/crudDIO/CrudDioApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.crudDIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/crudDIO/CrudDioApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.crudDIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
