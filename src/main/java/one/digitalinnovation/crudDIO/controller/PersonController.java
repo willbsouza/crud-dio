f7d558beb895dada6b75cbd22608c205f23514ce
Willams Souza <willamsbernardo@hotmail.com>
2021-10-31 13:19:51 -0300

    Developed list all method

--- src/main/java/one/digitalinnovation/crudDIO/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/crudDIO/controller/PersonController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -26,4 +27,8 @@ public class PersonController {
         return personService.createPerson(personDTO);
     }
 
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
+    }
 }
